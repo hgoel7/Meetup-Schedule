65154ff35440288985a01af6f7a943ac8a641514
Hanish Goel <hanish.goel@careerbuilder.com>
2020-09-13 20:26:13 +0530

    Code publish

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace Meetup_Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstDayCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> firstDay = new List<int>();
+
+            for (int i = 0; i < firstDayCount; i++)
+            {
+                int firstDayItem = Convert.ToInt32(Console.ReadLine().Trim());
+                firstDay.Add(firstDayItem);
+            }
+
+            int lastDayCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> lastDay = new List<int>();
+
+            for (int i = 0; i < lastDayCount; i++)
+            {
+                int lastDayItem = Convert.ToInt32(Console.ReadLine().Trim());
+                lastDay.Add(lastDayItem);
+            }
+
+            int result = Result.countMeetings(firstDay, lastDay);
+            Console.WriteLine("Result : " + result);
+
+        }
+    }
+}
+
+
+class Result
+{
+    //private static List<int> appointmentSchedule;
+
+    /*
+     * Complete the 'countMeetings' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY firstDay
+     *  2. INTEGER_ARRAY lastDay
+     */
+
+    public static int countMeetings(List<int> firstDay, List<int> lastDay)
+    {   
+        int length = 0;
+        
+        length = firstDay.Count();
+        
+        Dictionary<int,int> dictData = new Dictionary<int, int>();
+
+        for(int i = 0; i< length; i++) {
+            int currFDay = firstDay.ElementAt(i);
+            int currLDay = lastDay.ElementAt(i);
+            bool saved = false;
+            while (!saved) {
+                if( !dictData.ContainsKey(currFDay) ) {
+                    dictData.Add(currFDay, currLDay);
+                    saved = true;
+                }
+                else if (currFDay < currLDay)
+                {
+                    currFDay++;
+                    continue;
+                }
+                else
+                {
+                    int lDay = dictData[currFDay];
+                    if( lDay == currFDay ) {
+                        saved = true;
+                    }
+                    else
+                    {
+                        dictData[currFDay] = currLDay;
+                        currLDay = lDay;
+                        continue;
+                    }
+                }
+            }
+        }
+        return dictData.Count;        
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Problems-Solutions
-Some of the problems I came across, and these are the solutions that I have to have written trying to solve maximum related test cases. 
+Question: A start-up owner is looking to meet new investors to get some funds for his company. Each investor has a tight schedule that the owner has to respect. Given the schedules of the days investors are available, determine how many meetings the owner can schedule. Note that the owner can only have one meeting per day.The schedules consists of two integer arrays, firstDay and lastDay. Each element in the array firstDay represents the first day an investor is available, and each element in lastDay represents the last day an investor is available, both inclusive.
+
+
+STDIN     Function
+------    --------
+5         firstDay[] size n = 5
+1    →    firstDay = [1, 2, 1, 2, 2]
+2
+1
+2
+2
+5    →    lastDay[] size n = 5
+3    →    lastDay = [3, 2, 1, 3, 3]
+
+Sample Output : 3
+
+
+Another Sample Data: 
+    firstDayCount = 5
+    firstDay = [1,2,1,2,2]
+    lastDayCount = 5
+    lastDay = [1,2,3,5,3]
+
+
+
+    Result : 4
